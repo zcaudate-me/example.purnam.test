df654d21a8b7e4450b19345fa939e647a15499cd
Chris Zheng <z@caudate.me>
2013-10-30 14:37:28 +1100

    Added Examples

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject purnam-karma-testing "0.0.0-SNAPSHOT"
   :description "Karma testing with clojurescript"
-  :url "http://docs.caudate.me/purnam-crafty-game"
+  :url "https://www.github.com/zcaudate/purnam-karma-testing"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
