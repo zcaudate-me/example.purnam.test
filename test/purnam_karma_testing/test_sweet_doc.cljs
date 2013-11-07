5b79c3628a30ffd5d1559c6ad59975a941e0b0d3
Chris Zheng <z@caudate.me>
2013-11-07 15:34:36 +1100

    refined midje-doc

--- test/purnam_karma_testing/test_sweet_doc.cljs
+++ test/purnam_karma_testing/test_sweet_doc.cljs
@@ -4,14 +4,18 @@
                [purnam.test :only [init describe is it]]
                [purnam.test.sweet :only [fact facts]]))
 
+[[{:hide true}]]
 (init)
 
 [[:chapter {:title "testing clojure"}]]
 
 [[:section {:title "'pure' clojurescript hashmaps"}]]
 
-(facts [[{:doc "Basic Hashmaps"
-          :globals [o {:a 1 :b 2 :c 3}]}]]
+"Remember, these are tests in clojurescript!"
+
+(def o {:a 1 :b 2 :c 3})
+
+(facts [[{:doc "Basic Hashmaps"}]]
   (o :a)       => 1 
   (o :a)       => #(not= 0 %)
   (get o :b)   => 2
@@ -21,29 +25,35 @@
 
 [[:section {:title "native javascript objects"}]]
 
+"We now look at doing tests on native javascript objects"
+
+(def o (obj :a 1 :b 2 :c 3))
+
 (facts [[{:doc "an example test description"
           :globals [ka "a"
                    kb "b"]
           :vars [o (obj :a 1 :b 2 :c 3)]}]]
-
- [[:subsection {:title "dot notation for native objects"}]]
+ 
+ "Here are some examples"
+ 
+ [[:subsubsection {:title "dot notation for native objects"}]]
  o.a => 1
  (+ o.a o.b o.c) => 6
 
 
- [[:subsection {:title "support for both native and cljs comparisons"}]]
+ [[:subsubsection {:title "support for both native and cljs comparisons"}]]
  o => (obj :a 1 :b 2 :c 3)
  [1 2 3 4] => [1 2 3 4]
 
- [[:subsection {:title "support for function comparison"}]]
+ [[:subsubsection {:title "support for function comparison"}]]
   2 => even?
   3 => (comp not even?)
 
-  [[:subsection {:title "globals can be accessed"}]]
+  [[:subsubsection {:title "globals can be accessed"}]]
   o.|ka| => 1
   (+ o.|ka| o.|kb|) => 3
 
-  [[:subsection {:title "vars can be rebound"}]]
+  [[:subsubsection {:title "vars can be rebound"}]]
   (! o (arr [1 2 3]
             [4 5 6]
             [7 8 9]))
