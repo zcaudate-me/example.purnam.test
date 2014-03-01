6bd53c432189b96aa93e795b0d4382689d26a454
Chris Zheng <chris.zheng@auspost.com.au>
2014-03-01 14:02:25 +1100

    v0.4.3

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licencses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [im.chit/purnam.test "0.4.1"]]
+                 [im.chit/purnam.test "0.4.3"]]
  :profiles {:dev {:dependencies [[org.clojure/clojurescript "0.0-2138"]
                                  [midje "1.6.0"]]
                   :plugins [[lein-cljsbuild "1.0.0"]
